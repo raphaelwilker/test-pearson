a11f9eacd8d76528d2c142508fb7cd00de1f05d9
Raphael Araujo/LGESP CNS - SSP(raphael.araujo@lgepartner.com) <raphael.araujo@lge.com>
2019-05-16 16:51:11 -0300

    master: adjustment in word with accentuation

--- public/menu.js
+++ public/menu.js
@@ -1,16 +1,16 @@
 d = new dTree('d');
 
 d.add(0,-1,'');
-d.add(1,0,'Geração de tensão alternada em diferentes posições','#','Introdução','',true);
-d.add(2,1,'Introdução','javascript: go(1)','Introdução');
-d.add(3,1,'Geração de tensão alternada em diferentes posições','javascript: go(2)','Geração de tensão alternada em diferentes posições');
+d.add(1,0,'GeraÃ§Ã£o de tensÃ£o alternada em diferentes posiÃ§Ãµes','#','IntroduÃ§Ã£o','',true);
+d.add(2,1,'IntroduÃ§Ã£o','javascript: go(1)','IntroduÃ§Ã£o');
+d.add(3,1,'GeraÃ§Ã£o de tensÃ£o alternada em diferentes posiÃ§Ãµes','javascript: go(2)','GeraÃ§Ã£o de tensÃ£o alternada em diferentes posiÃ§Ãµes');
 
-d.add(4,0, 'Unidade de medida de frequência','#', 'Unidade de medida de frequência','',true);
-d.add(5,4, 'Unidade de medida de frequência', 'javascript: go(3)', 'Unidade de medida de frequência');
+d.add(4,0, 'Unidade de medida de frequÃªncia','#', 'Unidade de medida de frequÃªncia','',true);
+d.add(5,4, 'Unidade de medida de frequÃªncia', 'javascript: go(3)', 'Unidade de medida de frequÃªncia');
 
-d.add(6,0, 'Resíduos resultantes da poda de árvores','#', 'Resíduos resultantes da poda de árvores','',true);
-d.add(7,6, 'Resíduos resultantes da poda de árvores', 'javascript: go(4)', 'Resíduos resultantes da poda de árvores');
+d.add(6,0, 'ResÃ­duos resultantes da poda de Ã¡rvores','#', 'ResÃ­duos resultantes da poda de Ã¡rvores','',true);
+d.add(7,6, 'ResÃ­duos resultantes da poda de Ã¡rvores', 'javascript: go(4)', 'ResÃ­duos resultantes da poda de Ã¡rvores');
 
 d.draw();
 
-//Exemplo   d.add(sequencia, nó pai, Nome, link, titulo(title, hover), target, aberto(true, false), imagem)
+//Exemplo   d.add(sequencia, nÃ³ pai, Nome, link, titulo(title, hover), target, aberto(true, false), imagem)
